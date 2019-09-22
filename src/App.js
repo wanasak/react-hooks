9cdc0ed76ea1f3b6065a16da8c29d5c51af1b246
Wanasak <u510610433@gmail.com>
2019-09-22 15:05:29 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+  const [news, setNews] = useState([]);
+  const [searchQuery, setsearchQuery] = useState('react');
+  const [url, setUrl] = useState(
+    `http://hn.algolia.com/api/v1/search?query=${searchQuery}`
+  );
+  const [loading, setLoading] = useState(false);
+
+  const fetchNews = () => {
+    setLoading(true);
+    fetch(url)
+      .then(result => result.json())
+      .then(data => setNews(data.hits))
+      .catch(error => console.log(error))
+      .finally(() => setLoading(false));
+  };
+
+  useEffect(() => {
+    fetchNews();
+  }, [url]);
+
+  const handleChange = e => {
+    setsearchQuery(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`);
+  };
+
+  const showLoading = () => (loading ? <h2>Loading...</h2> : null);
+
+  const showNews = () => news.map((n, i) => <p key={i}>{n.title}</p>);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>News</h2>
+      {showLoading()}
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={searchQuery} onChange={handleChange} />
+        <button>Search</button>
+      </form>
+      {showNews()}
     </div>
   );
-}
+};
 
 export default App;
